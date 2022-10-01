6d3efddb170a3b5409cd424bff6fcadd9575d571
AmjadAyoub <amjadayoub1976@gmail.com>
2022-10-01 13:48:17 -0400

    Batch14IntelliJ

---
+++ src/class14/MethodsDemo1.java
@@ -0,0 +1,13 @@
+package class14;
+
+public class MethodsDemo1 {
+    void printHello() {
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args) {
+        MethodsDemo1 obj=new MethodsDemo1();
+        obj.printHello();
+    }
+}
