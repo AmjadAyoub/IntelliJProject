45e48259c8921010214ed0c016f85feecc26d6a1
AmjadAyoub <amjadayoub1976@gmail.com>
2022-11-12 15:38:16 -0500

    Batch14IntelliJ

--- src/Replits/Replit205.java
+++ src/Replits/Replit205.java
@@ -1,25 +1,21 @@
 package Replits;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class Replit205 {
     public static void main(String[] args) {
-        HashMap<String,String> address=new HashMap<>();
-        address.put("Street","Patrick ST");
-        address.put("Suite","265");
-        address.put("City","Vienna");
-        address.put("Zip","22180");
-        address.put("Country","United State");
-        Iterator iterator = address.entrySet().iterator();
-        while(iterator.hasNext()) {
-            Map.Entry m= (Map.Entry) iterator.next();
-            m.getValue();
-         String list= m.toString().toUpperCase();
-            System.out.println(list);
+        HashMap<String, String> address = new HashMap<>();
+        address.put("Street", "Patrick ST");
+        address.put("Suite", "265");
+        address.put("City", "Vienna");
+        address.put("Zip", "22180");
+        address.put("Country", "United State");
+        var iterator = address.entrySet().iterator();
 
-
+        while (iterator.hasNext()) {
+            var item = iterator.next();
+            var value = item.getValue();
+            System.out.println(value.toUpperCase());
 
 
         }
